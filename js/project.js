6cca17be5ad5cde50472f838996db214a5001d06
kuangkeng <kuangkeng@gmail.com>
2020-01-29 23:47:39 +0800

    map added

--- js/project.js
+++ js/project.js
@@ -209,6 +209,40 @@ $(document).ready(function() {
         offset: "50%"
     });
 
+    $("#scroll-text-9").waypoint(function(direction) {
+        if (direction === "down") {
+            $("#chart-wrapper-4").fadeTo(500, 1, makeChart4());
+        } else {}
+    }, {
+        offset: "50%"
+    });
+
+    $("#scroll-text-10").waypoint(function(direction) {
+        if (direction === "down") {
+            chart4.get("latinoMap").points[43].update({borderWidth: 10,borderColor: '#FF9600'});
+        } else {}
+    }, {
+        offset: "50%"
+    });
+    $("#scroll-text-11").waypoint(function(direction) {
+        if (direction === "down") {
+            chart4.get("latinoMap").points[43].update({borderWidth: 0});
+            chart4.get("latinoMap").points[9].update({borderWidth: 10,borderColor: '#FF9600'});
+        } else {}
+    }, {
+        offset: "50%"
+    });
+
+    $("#scroll-text-12").waypoint(function(direction) {
+        if (direction === "down") {
+            $("#sticky-box-4").animate({opacity: 0},1000);
+        } else {
+            $("#sticky-box-4").animate({opacity: 1},1000);
+        }
+    }, {
+        offset: "100%"
+    });
+
     // $("#scroll-text-10").waypoint(function(direction) {
     //     if (direction === "down") {
     //         chart3a.addSeries(dataGrowthNum2[1][0]);
@@ -751,39 +785,435 @@ $(document).ready(function() {
         });
     }
 
-    function makeChart3a() {
-        chart3a = new Highcharts.Chart({
+    function makeChart4() {
+        chart4 = new Highcharts.Chart({
             chart: {
-                renderTo: 'chart-3',
-                type: 'line',
-                marginRight: 70
+                renderTo: 'chart-4',
+                type: 'tilemap',
+                inverted: true,
+                // height: '80%'
             },
             title: {text: null},
             subtitle: {enabled: false},
-            xAxis: {
-                endOnTick: false,
-                labels: {style:{fontSize: '12px'}},
-                tickPositions: [2005, 2010, 2015],
-            },  
-            yAxis: {
-                title: {text: 'Number of eligible voters'},
-                endOnTick: true,
-                labels: {style:{fontSize: '12px'}},
-                min: 0,
+            xAxis: {visible: false},
+            yAxis: {visible: false},
+            colorAxis: {
+                dataClasses: [{
+                    from: 0,
+                    to: 5,
+                    color: '#ffffb2',
+                    name: '5% or less'
+                }, {
+                    from: 5.1,
+                    to: 10,
+                    color: '#fed976',
+                    name: '5.1-10%'
+                }, {
+                    from: 10.1,
+                    to: 15,
+                    color: '#feb24c',
+                    name: '10.1-15%'
+                }, {
+                    from: 15.1,
+                    to: 20,
+                    color: '#fd8d3c',
+                    name: '15.1-20%'
+                }, {
+                    from: 20.1,
+                    to: 25,
+                    color: '#f03b20',
+                    name: '20.1-25%'
+                }, {
+                    from: 25.1,
+                    color: '#bd0026',
+                    name: '>25%'
+                }]
             },
             credits: {enabled: false},
-            legend: {enabled: false},
-            tooltip: {enabled: true},
+            legend: {
+                enabled: true,
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'bottom',
+                floating: true,
+            },
+            tooltip: {enabled: false},
             plotOptions: {
-                series:{
-                    dataLabels: {enabled: false},
-                    pointStart: 2001,
-                    marker: {enabled: false},
-                },
-            },    
-            series: dataGrowthNum2[0]
+                series: {
+                    dataLabels: {
+                        enabled: true,
+                        format: '{point.hc-a2}',
+                        color: '#333',
+                        style: {
+                            textOutline: false
+                        }
+                    }
+                }
+            },   
+            series: [{
+                // pointPadding: 0,
+                name: 'latinoMap',
+                id: 'latinoMap',
+                data: [{
+                    'hc-a2': 'AL',
+                    name: 'Alabama',
+                    region: 'South',
+                    x: 6,
+                    y: 7,
+                    value: 2.3
+                }, {
+                    'hc-a2': 'AK',
+                    name: 'Alaska',
+                    region: 'West',
+                    x: 0,
+                    y: 0,
+                    value: 6.1
+                }, {
+                    'hc-a2': 'AZ',
+                    name: 'Arizona',
+                    region: 'West',
+                    x: 5,
+                    y: 3,
+                    value: 24.2
+                }, {
+                    'hc-a2': 'AR',
+                    name: 'Arkansas',
+                    region: 'South',
+                    x: 5,
+                    y: 6,
+                    value: 4.0
+                }, {
+                    'hc-a2': 'CA',
+                    name: 'California',
+                    region: 'West',
+                    x: 5,
+                    y: 2,
+                    value: 31.2
+                }, {
+                    'hc-a2': 'CO',
+                    name: 'Colorado',
+                    region: 'West',
+                    x: 4,
+                    y: 3,
+                    value: 16.4
+                }, {
+                    'hc-a2': 'CT',
+                    name: 'Connecticut',
+                    region: 'Northeast',
+                    x: 3,
+                    y: 11,
+                    value: 12.6
+                }, {
+                    'hc-a2': 'DE',
+                    name: 'Delaware',
+                    region: 'South',
+                    x: 4,
+                    y: 9,
+                    value: 5.7
+                }, {
+                    'hc-a2': 'DC',
+                    name: 'District of Columbia',
+                    region: 'South',
+                    x: 4,
+                    y: 10,
+                    value: 7.1
+                }, {
+                    'hc-a2': 'FL',
+                    name: 'Florida',
+                    region: 'South',
+                    x: 8,
+                    y: 8,
+                    value: 20.7
+                }, {
+                    'hc-a2': 'GA',
+                    name: 'Georgia',
+                    region: 'South',
+                    x: 7,
+                    y: 8,
+                    value: 5.3
+                }, {
+                    'hc-a2': 'HI',
+                    name: 'Hawaii',
+                    region: 'West',
+                    x: 8,
+                    y: 0,
+                    value: 9.0
+                }, {
+                    'hc-a2': 'ID',
+                    name: 'Idaho',
+                    region: 'West',
+                    x: 3,
+                    y: 2,
+                    value: 8.4
+                }, {
+                    'hc-a2': 'IL',
+                    name: 'Illinois',
+                    region: 'Midwest',
+                    x: 3,
+                    y: 6,
+                    value: 12.0
+                }, {
+                    'hc-a2': 'IN',
+                    name: 'Indiana',
+                    region: 'Midwest',
+                    x: 3,
+                    y: 7,
+                    value: 4.3
+                }, {
+                    'hc-a2': 'IA',
+                    name: 'Iowa',
+                    region: 'Midwest',
+                    x: 3,
+                    y: 5,
+                    value: 3.6
+                }, {
+                    'hc-a2': 'KS',
+                    name: 'Kansas',
+                    region: 'Midwest',
+                    x: 5,
+                    y: 5,
+                    value: 7.3
+                }, {
+                    'hc-a2': 'KY',
+                    name: 'Kentucky',
+                    region: 'South',
+                    x: 4,
+                    y: 6,
+                    value: 1.7
+                }, {
+                    'hc-a2': 'LA',
+                    name: 'Louisiana',
+                    region: 'South',
+                    x: 6,
+                    y: 5,
+                    value: 3.1
+                }, {
+                    'hc-a2': 'ME',
+                    name: 'Maine',
+                    region: 'Northeast',
+                    x: 0,
+                    y: 11,
+                    value: 1.5
+                }, {
+                    'hc-a2': 'MD',
+                    name: 'Maryland',
+                    region: 'South',
+                    x: 4,
+                    y: 8,
+                    value: 6.0
+                }, {
+                    'hc-a2': 'MA',
+                    name: 'Massachusetts',
+                    region: 'Northeast',
+                    x: 2,
+                    y: 10,
+                    value: 9.2
+                }, {
+                    'hc-a2': 'MI',
+                    name: 'Michigan',
+                    region: 'Midwest',
+                    x: 2,
+                    y: 7,
+                    value: 3.6
+                }, {
+                    'hc-a2': 'MN',
+                    name: 'Minnesota',
+                    region: 'Midwest',
+                    x: 2,
+                    y: 4,
+                    value: 3.3
+                }, {
+                    'hc-a2': 'MS',
+                    name: 'Mississippi',
+                    region: 'South',
+                    x: 6,
+                    y: 6,
+                    value: 1.7
+                }, {
+                    'hc-a2': 'MO',
+                    name: 'Missouri',
+                    region: 'Midwest',
+                    x: 4,
+                    y: 5,
+                    value: 2.8
+                }, {
+                    'hc-a2': 'MT',
+                    name: 'Montana',
+                    region: 'West',
+                    x: 2,
+                    y: 2,
+                    value: 3.5
+                }, {
+                    'hc-a2': 'NE',
+                    name: 'Nebraska',
+                    region: 'Midwest',
+                    x: 4,
+                    y: 4,
+                    value: 6.8
+                }, {
+                    'hc-a2': 'NV',
+                    name: 'Nevada',
+                    region: 'West',
+                    x: 4,
+                    y: 2,
+                    value: 20.2
+                }, {
+                    'hc-a2': 'NH',
+                    name: 'New Hampshire',
+                    region: 'Northeast',
+                    x: 1,
+                    y: 11,
+                    value: 2.9
+                }, {
+                    'hc-a2': 'NJ',
+                    name: 'New Jersey',
+                    region: 'Northeast',
+                    x: 3,
+                    y: 10,
+                    value: 15.5
+                }, {
+                    'hc-a2': 'NM',
+                    name: 'New Mexico',
+                    region: 'West',
+                    x: 6,
+                    y: 3,
+                    value: 43.3
+                }, {
+                    'hc-a2': 'NY',
+                    name: 'New York',
+                    region: 'Northeast',
+                    x: 2,
+                    y: 9,
+                    value: 15.0
+                }, {
+                    'hc-a2': 'NC',
+                    name: 'North Carolina',
+                    region: 'South',
+                    x: 5,
+                    y: 9,
+                    value: 4.7
+                }, {
+                    'hc-a2': 'ND',
+                    name: 'North Dakota',
+                    region: 'Midwest',
+                    x: 2,
+                    y: 3,
+                    value: 3.1
+                }, {
+                    'hc-a2': 'OH',
+                    name: 'Ohio',
+                    region: 'Midwest',
+                    x: 3,
+                    y: 8,
+                    value: 2.8
+                }, {
+                    'hc-a2': 'OK',
+                    name: 'Oklahoma',
+                    region: 'South',
+                    x: 6,
+                    y: 4,
+                    value: 6.4
+                }, {
+                    'hc-a2': 'OR',
+                    name: 'Oregon',
+                    region: 'West',
+                    x: 4,
+                    y: 1,
+                    value: 8.0
+                }, {
+                    'hc-a2': 'PA',
+                    name: 'Pennsylvania',
+                    region: 'Northeast',
+                    x: 3,
+                    y: 9,
+                    value: 5.5
+                }, {
+                    'hc-a2': 'RI',
+                    name: 'Rhode Island',
+                    region: 'Northeast',
+                    x: 2,
+                    y: 11,
+                    value: 11.7
+                }, {
+                    'hc-a2': 'SC',
+                    name: 'South Carolina',
+                    region: 'South',
+                    x: 6,
+                    y: 8,
+                    value: 3.2
+                }, {
+                    'hc-a2': 'SD',
+                    name: 'South Dakota',
+                    region: 'Midwest',
+                    x: 3,
+                    y: 4,
+                    value: 2.8
+                }, {
+                    'hc-a2': 'TN',
+                    name: 'Tennessee',
+                    region: 'South',
+                    x: 5,
+                    y: 7,
+                    value: 2.6
+                }, {
+                    'hc-a2': 'TX',
+                    name: 'Texas',
+                    region: 'South',
+                    x: 7,
+                    y: 4,
+                    value: 31.0
+                }, {
+                    'hc-a2': 'UT',
+                    name: 'Utah',
+                    region: 'West',
+                    x: 5,
+                    y: 4,
+                    value: 9.4
+                }, {
+                    'hc-a2': 'VT',
+                    name: 'Vermont',
+                    region: 'Northeast',
+                    x: 1,
+                    y: 10,
+                    value: 1.6
+                }, {
+                    'hc-a2': 'VA',
+                    name: 'Virginia',
+                    region: 'South',
+                    x: 5,
+                    y: 8,
+                    value: 5.7
+                }, {
+                    'hc-a2': 'WA',
+                    name: 'Washington',
+                    region: 'West',
+                    x: 2,
+                    y: 1,
+                    value: 8.0
+                }, {
+                    'hc-a2': 'WV',
+                    name: 'West Virginia',
+                    region: 'South',
+                    x: 4,
+                    y: 7,
+                    value: 1.1
+                }, {
+                    'hc-a2': 'WI',
+                    name: 'Wisconsin',
+                    region: 'Midwest',
+                    x: 2,
+                    y: 5,
+                    value: 4.3
+                }, {
+                    'hc-a2': 'WY',
+                    name: 'Wyoming',
+                    region: 'West',
+                    x: 3,
+                    y: 3,
+                    value: 7.7
+                }]
+            }]
         });
-        addLabel3a('Total');
     }
 
     function makeChart3b() {
@@ -906,7 +1336,7 @@ $(document).ready(function() {
         addLabel3d('White');
     }
 
-    function makeChart4() {
+    function makeChart() {
         chart4 = new Highcharts.Chart({
             chart: {
                 renderTo: 'chart-4',
